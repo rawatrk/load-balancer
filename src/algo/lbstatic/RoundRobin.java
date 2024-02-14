905c15b461afbd4d7e59892ef1d052599db281e5
rawatrk <ritesh@onetick.in>
2024-02-14 11:40:53 +0530

    first commit

---
+++ src/algo/lbstatic/RoundRobin.java
@@ -0,0 +1,12 @@
+package algo.lbstatic;
+
+
+public class RoundRobin {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+}
---
+++ src/algo/lbstatic/SourceIpHash.java
@@ -0,0 +1,10 @@
+package algo.lbstatic;
+
+public class SourceIpHash {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
