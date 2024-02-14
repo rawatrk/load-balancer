c1a928a05f19c17272e63acfc179121ebfe42b43
rawatrk <ritesh@onetick.in>
2024-02-14 11:46:25 +0530

    second commit

---
+++ load-balancer-algo/src/algo/lbdynamic/LeastResponse.java
@@ -0,0 +1,10 @@
+package algo.lbdynamic;
+
+public class LeastResponse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ load-balancer-algo/src/algo/lbstatic/WeightedRoundRobin.java
@@ -0,0 +1,10 @@
+package algo.lbstatic;
+
+public class WeightedRoundRobin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
